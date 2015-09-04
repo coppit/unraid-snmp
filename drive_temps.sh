4f63ea337136a6ef3c2d09c1aa1fa88ff46bfd0b
David Coppit <david@coppit.org>
2015-09-03 21:46:56 -0400

    Disable standby check
    Guzzi reported on the forums that it prevented the disk temps from being collected.
    I tested it on my system and smartctl didn't spin up the drives.
    https://lime-technology.com/forum/index.php?topic=41019.msg403695#msg403695

--- drive_temps.sh
+++ drive_temps.sh
@@ -34,16 +34,19 @@ do
   if [[ "$device_num" != "$name_num" ]]
   then
     echo 'ERROR! Couldn'"'"'t parse mdcmd output. Command was:'
-    echo 'mdcmd status | $GREP '"'"'\(rdevId\|rdevName\).*=.'"'"' | while read -r device'
+    echo '$MDCMD status | $GREP '"'"'\(rdevId\|rdevName\).*=.'"'"' | while read -r device'
   fi
 
   device=$(echo $device | $SED 's#.*=#/dev/#')
   name=$(echo $name | $SED 's/.*=//')
 
-  if ! $HDPARM -C $device 2>&1 | $GREP -cq standby
-  then
+  # Guzzi reports that it doesn't work for him with this guard code. I tested it and on my systems the drives don't spin
+  # up. Perhaps this behavior changed from when the code I stole was first written. :)
+  # https://lime-technology.com/forum/index.php?topic=41019.msg403695#msg403695
+#  if ! $HDPARM -C $device 2>&1 | $GREP -cq standby
+#  then
     temp=$($SMARTCTL -A $device | $GREP -m 1 -i Temperature_Celsius | $AWK '{print $10}')
-  fi
+#  fi
 
   # For debugging
 #  echo "$name = $device, $temp"
